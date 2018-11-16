86063215e0091946a7efd31b4e1a018641ae6983
Guillaume <guillaume.mande56@gmail.com>
2018-11-16 11:17:25 +0100

    Voir changelog.mod (version 0.3.6)

--- src/pdl/wiki/HTMLExtractor.java
+++ src/pdl/wiki/HTMLExtractor.java
@@ -1,18 +1,39 @@
 package pdl.wiki;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class HTMLExtractor implements Extractor
 {
-    private ArrayList<String> genCSVList()
-    {
-        return null;
-    }
+    private ArrayList<String> genCSVList;
 
     @Override
     public ArrayList<String> getCSV(Url purl)
     {
-        return null;
-    }
+        String url = purl.toString();
+        ArrayList<String> csvData = new ArrayList<>();
 
+        try{
+            Document doc = Jsoup.connect(url).get();
+            Element table = doc
+                    .select("div.mainwrapper div.main_background div.main_left")
+                    .get(0);
+            Elements rows = table .select("tr");
+            csvData.add(rows.get(0).select("th").toString());
+
+            for(int j = 1; j < rows.size(); j++){
+                csvData.add(rows.get(j).select("td").toString());
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return csvData;
+    }
 }
