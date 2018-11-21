7c52213531e9f38b432061f7815a330f6e66ddcd
Thibaut Legroux <thibaut.legroux@gmail.com>
2018-11-21 16:29:09 +0100

    Voir changelog.md (0.4.1) pour les informations de version.

--- src/pdl/test/PageTest.java
+++ src/pdl/test/PageTest.java
@@ -1,11 +1,17 @@
 package pdl.test;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import pdl.wiki.Page;
 import pdl.wiki.*;
 import org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -15,19 +21,51 @@ import static org.junit.Assert.*;
 public class PageTest
 {
 
-    @Test
-    public void getUrl()
+    private static String creationLien(String filePath)
     {
-        Url url_test = new Url("https://fr.wikipedia.org/wiki/Nombre_premier");
-        ArrayList array_test = new ArrayList();
-        array_test.add("Liste Test 1");
-        array_test.add("Liste Test 2");
-        Page page_test = new Page(url_test,array_test);
-        String test = page_test.getUrl().getLink();
+        StringBuilder contentBuilder = new StringBuilder();
 
-        assertTrue("Fonction getUrl()",test.equals("https://fr.wikipedia.org/wiki/Nombre_premier"));
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s->contentBuilder.append("https://wikipedia.org/wiki/").append(s).append("\n"));
 
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return contentBuilder.toString();
     }
+
+    @Test
+    public void getUrl() throws IOException {
+
+        ArrayList lien = new ArrayList();
+
+        String lien_string = this.creationLien("inputdata/wikiurls.txt");
+       // System.out.println("Premier : "+lien_string);
+        int nblignes = StringUtils.countMatches(lien_string,"\n");
+        //System.out.println("nb lignes : "+nblignes);
+        for (int i =0;i<=nblignes;i++){
+            lien.add(lien_string.split("$"));
+        }
+        for(int i =0;i<lien.size();i++) {
+            System.out.println("Case ["+i+"]"+lien.get(i));
+        }
+
+           /* Url url_test = new Url(s.toString());
+            ArrayList array_test = new ArrayList();
+            array_test.add("Liste Test 1");
+            array_test.add("Liste Test 2");
+            Page page_test = new Page(url_test,array_test);
+            String test = page_test.getUrl().getLink();
+
+            assertTrue("Fonction getUrl()",test.equals("https://fr.wikipedia.org/wiki/"+s));*/
+
+
+        }
+
 
     @Test
     public void getTitle()
